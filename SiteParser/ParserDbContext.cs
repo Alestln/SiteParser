16efeee8d1dd5c0802557d3816c97da6cf369c6e
Alestln <itworkplace46@gmail.com>
2024-05-07 22:02:17 +0300

    [FEATURE] added saving data in specify scheme db

--- SiteParser/ParserDbContext.cs
+++ SiteParser/ParserDbContext.cs
@@ -5,18 +5,30 @@ namespace SiteParser;
 
 public class ParserDbContext : DbContext
 {
+    private const string DbSchema = "refactoring_guru";
+    private const string DbMigrationsHistoryTable = "__RefactoringGuruDbMigrationsHistory";
+    
     public DbSet<Article> Articles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        var connectionString = "Server=localhost;Port=5432;Database=site_parser;Username=postgres;Password=postgres";
         optionsBuilder
-            .UseNpgsql("Server=localhost;Port=5432;Database=parser;Username=postgres;Password=123456")
+            .UseNpgsql(connectionString,
+                npgsqlOptions =>
+                {
+                    npgsqlOptions.MigrationsHistoryTable(
+                        DbMigrationsHistoryTable,
+                        DbSchema);
+                })
             .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
     }
     
     // Optionally, you can override the OnModelCreating method to configure the model
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.HasDefaultSchema(DbSchema);
+        
         modelBuilder.Entity<Article>()
             .HasMany(a => a.InternalLinks)
             .WithOne()
